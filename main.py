248c3f49ef8a2a4c739662da87231c55ea02bacf
Futaba-Kosuke <k.futaba2828@gmail.com>
2021-04-10 01:48:50 +0900

    Add: エラー処理とコメントを追加

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ SLOW_SPEED: Final[int] = 10 * 1e3
 SLOW_SPEED_TIME: Final[int] = 1 * 60 + 30
 SLOW_SPEED_MONEY: Final[int] = 80
 
+# 入力を整形
 def parse_inputs (lines: List['str']) -> List[List[int]]:
 
     times_sec = [0] * len(lines)
@@ -28,11 +29,16 @@ def parse_inputs (lines: List['str']) -> List[List[int]]:
         time, distance = line.split()
         hour, minute, sec = time.split(':')
 
+        if float(hour) > 99 or float(minute) > 60 or float(sec) > 60:
+            raise Exception("Doesn't fit the time's constraints.")
+
         times_sec[i] = float(hour) * 60 * 60 + float(minute) * 60 + float(sec)
         distances_meter[i] = float(distance)
 
     return times_sec, distances_meter
 
+# 深夜か否か判定
+# 通常 - 深夜を跨ぐ場合でも深夜時間帯に走っていた距離が不明となるため、区間の始めと終わりが深夜区間に含まれている場合のみ深夜で判定
 def is_midnight (t_begin_sec: int, t_end_sec: int) -> bool:
     return (
             int((t_begin_sec % (24*60*60))/(60*60)) < 5 or 22 <= int((t_begin_sec % (24*60*60))/(60*60))
@@ -77,7 +83,10 @@ def calc_money (times_sec: List[float], distances_meter: List[float]) -> int:
 def main(lines: List['str']) -> None:
 
     # 入力のデータ形式を調整
-    times_sec, distances_meter = parse_inputs(lines)
+    try:
+        times_sec, distances_meter = parse_inputs(lines)
+    except:
+        sys.exit(1)
 
     # 料金を計算
     result = calc_money(times_sec, distances_meter)
